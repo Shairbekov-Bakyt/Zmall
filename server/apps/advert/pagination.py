9ac3a6c6255e1f712536b9e01d098fa7a7e9b7f6
Zharkyn20 <61348606+Zharkyn20@users.noreply.github.com>
2022-09-12 10:26:41 +0600

    update pagination, add promote fields

---
+++ server/apps/advert/pagination.py
@@ -0,0 +1,9 @@
+from rest_framework import pagination
+
+
+class AdvertPagination(pagination.LimitOffsetPagination):
+    default_limit = 25
+    max_limit = 50
+    min_limit = 1
+    min_offset = 1
+    max_offset = 50
--- server/apps/advert/views/advert_views.py
+++ server/apps/advert/views/advert_views.py
@@ -5,15 +5,13 @@ from rest_framework import status, filters
 from rest_framework.generics import ListAPIView
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
-from rest_framework import pagination
 
 
 from advert.serializers import advert_serializers as serializers
 from advert.models import Advert, AdvertImage, City, AdvertContact
-
 from advert.serializers import permissions
 from phonenumber_field.validators import validate_international_phonenumber
-
+from advert.pagination import AdvertPagination
 
 class AdvertFilter(django_filters.FilterSet):
     min_price = django_filters.NumberFilter(field_name="start_price", lookup_expr="gte")
@@ -35,7 +33,7 @@ class CityListView(ListAPIView):
 class AdvertViewSet(ModelViewSet):
     queryset = Advert.objects.filter(status="act")
     serializer_class = serializers.AdvertCreateSerializer
-    pagination_class = pagination.LimitOffsetPagination
+    pagination_class = AdvertPagination
     filter_backends = [
         django_filters.rest_framework.DjangoFilterBackend,
         filters.OrderingFilter, filters.SearchFilter,
