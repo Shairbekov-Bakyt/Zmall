358874df39e907fa7ed7bdd961049a4a56f191c4
c0mrade <cyberstalker@gmail.com>
2022-09-21 15:46:17 +0300

    bug_fix/pagination

--- server/apps/advert/api/pagination.py
+++ server/apps/advert/api/pagination.py
@@ -3,7 +3,7 @@ from rest_framework import pagination
 
 class AdvertPagination(pagination.LimitOffsetPagination):
     default_limit = 25
-    max_limit = 50
+    max_limit = 1000
     min_limit = 1
     min_offset = 1
     max_offset = 50
