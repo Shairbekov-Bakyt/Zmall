0111f3d0bd7f50a4523809aa070ac256076fcf85
c0mrade <cyberstalker@gmail.com>
2022-09-29 18:35:34 +0300

    server changes

--- server/apps/advert/utils.py
+++ server/apps/advert/utils.py
@@ -32,7 +32,7 @@ def generate_sig(data: dict, method: str) -> dict:
         pg_amount=data.price,
         pg_description=data.description,
         pg_salt=Util.get_random_string(30),
-        pg_success_url='http://188.225.83.42/swagger/',
+        pg_success_url='http://localhost:3000',
     )
     data = OrderedDict(sorted(data.items()))
     string = method
