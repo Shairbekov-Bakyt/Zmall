975082179efc100d9ffae1278a373b603f5f48b8
Zharkyn20 <61348606+Zharkyn20@users.noreply.github.com>
2022-09-29 12:20:48 +0600

    admin user

--- server/apps/custom_admin/views.py
+++ server/apps/custom_admin/views.py
@@ -1,20 +1,24 @@
 import django_filters
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAdminUser
-from rest_framework.generics import ListAPIView
 
-from advert.api.advert_serializers import CustomUser
+from user.models import CustomUser
+from user.api.serializers import RegisterSerializer
 from advert.views.advert_view import AdvertViewSet
-from custom_admin.serializers import UserSerializerAD, AdReportSerializer, AdminAdvertSerializer
+from custom_admin.serializers import UserSerializerAD, AdReportSerializer
 from advert.api.pagination import AdvertPagination
 from advert.models import Advert, AdvertReport
 
 
 class UserViewSet(ModelViewSet):
-    serializer_class = UserSerializerAD
+    serializer_class = RegisterSerializer
     queryset = CustomUser.objects.all()
     permission_classes = [IsAdminUser]
-    http_method_names = ["get", "put", "delete"]
+
+    def get_serializer_class(self):
+        if self.action == "list" or self.action == "retrieve":
+            return UserSerializerAD
+        return super().get_serializer_class()
 
 
 class AdvertFilter(django_filters.FilterSet):
